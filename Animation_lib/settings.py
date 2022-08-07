b388cae4a8e228a25cf7fe5fb7be86f0f19c2593
mustybalogs <kylejbalogs01@yahoo.com>
2022-08-07 23:27:16 +0100

    sigin in and register authorization activated

--- Animation_lib/settings.py
+++ Animation_lib/settings.py
@@ -79,10 +79,17 @@ WSGI_APPLICATION = 'Animation_lib.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'animaxdatabase',
+        'USER': 'root',
+        'HOST': '127.0.0.1',
+        'PORT': 3306,
+        'PASSWORD':'Mustybalogs-1'
     }
 }
+
+# Password validation
+# https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
 
 
 # Password validation
