e660b6b4926641db764c9473ae14724730c47925
Desomm <charlesdera777@gmail.com>
2022-08-09 09:16:04 +0100

    all password reset pages that would be used when user forgets password

--- animax/signin/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-from django.template import loader
-
-# Create your views here.
-def signin(request):
-  template = loader.get_template('signin.html')
-  return HttpResponse(template.render())
