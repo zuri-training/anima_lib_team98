f16ee4e7e3fea7ea96e286236b1898ac049e2bd1
David <davidalatishe@gmail.com>
2022-08-10 10:19:27 +0100

    Updated Login/Signup Pages

---
+++ Login_signup_FE_UPDATED/base/forms.py
@@ -0,0 +1,49 @@
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+
+class Signup(UserCreationForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['username'].widget.attrs.update({
+            'required': '',
+            'name':'username',
+            'type' : 'text',
+            
+            'placeholder' : 'JohnDoe',
+            'maxlength' : '16',
+            'minlength' : '4' ,
+            'id' : 'username'
+        })
+        self.fields['email'].widget.attrs.update({
+            'required': '',
+            'name':'email',
+            'type' : 'email',
+            
+            'placeholder' : 'JohnDoe@email.com',
+            'id' : 'email'
+        })
+        self.fields['password1'].widget.attrs.update({
+            'required': '',
+            'name':'password1',
+            'id' : 'password1',
+            'type' : 'password',
+            
+            'placeholder' : '********',
+            'maxlength' : '22',
+            'minlength' : '8' 
+        })
+        self.fields['password2'].widget.attrs.update({
+            'required': '',
+            'name':'password2',
+            'id' : 'password2',
+            'type' : 'password',
+            
+            'placeholder' : '********',
+            'maxlength' : '22',
+            'minlength' : '8' 
+        })
+
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password1', 'password2']
+
---
+++ Login_signup_FE_UPDATED/base/views.py
@@ -0,0 +1,63 @@
+
+from django.shortcuts import render, redirect
+from .forms import Signup
+from django.contrib import messages
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.models import User
+
+# Create your views here.
+
+
+def home(request):
+    return render(request, 'Animation_lib/home.html')
+
+def signupform(request):
+    if request.method == 'POST':
+        form = Signup(request.POST)
+
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            password = form.cleaned_data.get('password2')
+
+            if password != raw_password:
+                messages.error(request, 'password do not match')
+            
+            else:
+                form.save()
+                new_user = authenticate(username = username, password = raw_password)
+                if new_user is not None:
+                    
+                    login(request, new_user)
+                    return redirect('home')
+                else:
+                    messages.error(request, 'Service TimedOut')
+        else:
+            messages.error(request, 'Check Your Details Carefully')
+    else:
+        form = Signup()
+    context = {'form' : form}
+    return render(request, 'Animation_lib/signup.html', context)
+
+
+def signinform(request):
+    if request.method == 'POST':
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+        username = User.objects.get(email = email.lower()).username
+
+        try:
+            user = User.objects.get(username = username)
+        except:
+            messages.error(request, 'user is not recognised')
+
+        user = authenticate(request, username = username, password = password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.error(request, 'Wrong Password')
+    
+    context = {} 
+    return render(request, 'Animation_lib/signin.html', context)
